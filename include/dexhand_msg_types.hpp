37af294d8ed87447ef2999c65b480d34a4181ab3
Trent Shumay <trent@iotdesignshop.com>
2024-04-11 17:00:33 -0700

    New header for enumerating message ID's

---
+++ include/dexhand_msg_types.hpp
@@ -0,0 +1,18 @@
+/***
+ * @file dexhand_msg_types.h
+ * @brief This file contains the message IDs for the dexhandv2_binary_msgs package.
+ * @copyright Copyright (c) 2024 IoT Design Shop Inc.
+*/
+
+#pragma once
+
+namespace dexhand {
+
+enum DexhandMsgID {
+    SERVO_STATUS_MSG = 0x01,
+    NUM_MSGS
+};
+
+
+} // namespace dexhand
+
